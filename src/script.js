1673925f814199406b66140f7681db043b944939
bluesatch <satchwerk@gmail.com>
2025-06-03 11:42:40 -0500

    added floor textures

--- src/script.js
+++ src/script.js
@@ -26,6 +26,23 @@ const floorNormalTexture = textureLoader.load('./floor/coast_sand_rocks_02_nor_g
 const floorDisplacementTexture = textureLoader.load('./floor/coast_sand_rocks_02_disp_1k.jpg')
 
 
+floorColorTexture.repeat.set(8, 8)
+floorARMTexture.repeat.set(8, 8)
+floorNormalTexture.repeat.set(8, 8)
+floorDisplacementTexture.repeat.set(8, 8)
+
+floorColorTexture.wrapS = THREE.RepeatWrapping
+floorARMTexture.wrapS = THREE.RepeatWrapping
+floorNormalTexture.wrapS = THREE.RepeatWrapping
+floorDisplacementTexture.wrapS = THREE.RepeatWrapping
+
+floorColorTexture.wrapT = THREE.RepeatWrapping
+floorARMTexture.wrapT = THREE.RepeatWrapping
+floorNormalTexture.wrapT = THREE.RepeatWrapping
+floorDisplacementTexture.wrapT = THREE.RepeatWrapping
+
+floorColorTexture.colorSpace = THREE.SRGBColorSpace
+
 /**
  * House
  */
@@ -123,11 +140,18 @@ for (let i = 0; i < 30; i++) {
 // Floor
 
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20, 20),
+    new THREE.PlaneGeometry(20, 20, 100, 100),
     new THREE.MeshStandardMaterial({
         alphaMap: floorAlphaTexture,
         transparent: true,
-        map: floorColorTexture
+        map: floorColorTexture,
+        aoMap: floorARMTexture,
+        roughnessMap: floorARMTexture,
+        metalnessMap: floorARMTexture,
+        normalMap: floorNormalTexture,
+        displacementMap: floorDisplacementTexture,
+        displacementScale: 0.3,
+        displacementBias: - 0.2
     })
 )
 
@@ -135,6 +159,9 @@ floor.rotation.x = - Math.PI * 0.5
 // floor.rotation.y = - Math.PI * 0.5
 
 scene.add(floor)
+
+gui.add(floor.material, 'displacementScale').min(0).max(1).step(0.001).name('floorDisplacementScale')
+gui.add(floor.material, 'displacementBias').min(-1).max(1).step(0.001).name('floorDisplacementBias')
 
 /**
  * Lights
